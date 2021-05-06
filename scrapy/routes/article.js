2b382a61c4efebc0e9ccdfb7c4322890d562a796
Nishant kumar <nishantsingh1308@gmail.com>
2021-05-06 15:40:59 +0530

    blogs

---
+++ scrapy/routes/article.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { listenerCount } = require("../models/article");
+let Article = require("../models/article");
+const router = express.Router();
+router.get("/new", (req, res) => {
+  res.render("articles/new", { article: new Article() });
+});
+router.get("/:id", (req, res) => {
+  //const article=Article.findById(req.params.id)
+  res.send(req.params.id);
+});
+
+router.post("/", async (req, res) => {
+  const article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  });
+  try {
+    article = await article.save();
+    res.redirect("/articles/${article.id}");
+  } catch (e) {
+    res.render("/articles/new", { article: article });
+  }
+});
+
+module.exports = router;
